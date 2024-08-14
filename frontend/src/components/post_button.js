82e9fa79b692b814a7b46dfceda26f022b8bc71a
s22025 <s22025@std.it-college.ac.jp>
2024-08-14 13:03:27 +0900

    投稿ボタンの変更諸々

--- frontend/src/components/post_button.js
+++ frontend/src/components/post_button.js
@@ -1,13 +1,19 @@
+"use client";
+
 import s from '../styles/post_button.module.css';
+import { useRouter } from "next/navigation";
 
 const PostButton = () => {
-    return(
-        <>
-            <button className={s.buttonA}>+</button>
-            <button className={s.buttonB}>投稿</button>
-            <button className={s.buttonC}>レビュー</button>
-        </>
-    )
+    const router = useRouter();
+
+   return (
+    <div className={s.buttonContainer}>
+        <button className={s.buttonA}>+</button>
+        <button className={s.buttonB} onClick={() => router.push('/')}>投稿</button>
+        <button className={s.buttonC} onClick={() => router.push('/')}>レビュー</button>
+    </div>
+);
+
 }
 
 export default PostButton;
