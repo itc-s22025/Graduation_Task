e2386efde01dee16ee5b67395370548dfe4fef7b
s22025 <s22025@std.it-college.ac.jp>
2024-08-14 16:22:55 +0900

    投稿ボタンの変更 transition

--- frontend/src/components/post_button.js
+++ frontend/src/components/post_button.js
@@ -2,13 +2,21 @@
 
 import s from '../styles/post_button.module.css';
 import { useRouter } from "next/navigation";
+import {useState} from "react";
 
 const PostButton = () => {
     const router = useRouter();
 
+    //state
+    const [isClicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(!isClicked);
+    }
+
    return (
     <div className={s.buttonContainer}>
-        <button className={s.buttonA}>+</button>
+        <button className={isClicked ? `${s.buttonA} ${s.buttonAFocus}` : s.buttonA} onClick={handleClick}>+</button>
         <button className={s.buttonB} onClick={() => router.push('/')}>投稿</button>
         <button className={s.buttonC} onClick={() => router.push('/')}>レビュー</button>
     </div>
