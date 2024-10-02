ef0331607bdc80e2fceff954695d6669345d26c1
s22025 <s22025@std.it-college.ac.jp>
2024-10-02 14:36:11 +0900

    replyとかの配置だけ

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -16,6 +16,23 @@ const Post = () => {
                         <p className={s.content}>text text text text    <br/>
                             excelのプライマー欲し〜
                         </p>
+                        <div className={s.flex}>
+                            <div className={s.flex}>
+                                <p className={s.reply}/>
+                                <p>0</p>
+                            </div>
+                            <div className={s.flex}>
+                                <p className={s.repost}>⇄</p>
+                                <p>0</p>
+                            </div>
+                            <div className={s.flex}>
+                                <p className={s.like}>♡</p>
+                                <p>0</p>
+                            </div>
+                            <div className={s.flex}>
+                                <p className={s.save}></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
