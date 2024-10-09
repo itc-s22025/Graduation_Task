c8186b75179f606aa813ef8b66538edf76694093
s22025 <s22025@std.it-college.ac.jp>
2024-10-09 10:43:17 +0900

    サインインページのUIとりあえず（２）

--- frontend/src/app/SignUp/personalColor.js
+++ frontend/src/app/SignUp/personalColor.js
@@ -10,6 +10,8 @@ const Pc = () => {
     return(
         <>
             <h1 className={s.question}>
+                <span className={s.letter}>Q</span>
+                <span className={s.letter}>. </span>
                 <span className={s.letter}>あ</span>
                 <span className={s.letter}>な</span>
                 <span className={s.letter}>た</span>
@@ -27,18 +29,14 @@ const Pc = () => {
             </h1>
             <form>
                 <div className={s.pcContainer}>
-                    <div className={s.flex}>
                         <button type="button" value="Spring" className={s.spring}>イエベ春</button>
                         <button type="button"　value="Summer" className={s.summer}>ブルベ夏</button>
-                    </div>
-                    <div className={s.flex}>
                         <button type="button" value="Autumn" className={s.autumn}>イエベ秋</button>
                         <button type="button" value="Winter" className={s.winter}>ブルベ冬</button>
-                    </div>
                 </div>
 
                 <div className={s.submitContainer}>
-                    <button className={s.next}>次へ</button>
+                    <button type="button" className={s.next}>次へ</button>
                     <p className={s.or}>or</p>
                     <input type="submit" value="わからないので診断してみる" className={s.toTest}/>
                 </div>
