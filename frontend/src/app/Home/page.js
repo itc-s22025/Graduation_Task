fc42d455164805c923b5c4ea221aba5f01e69417
s22025 <s22025@std.it-college.ac.jp>
2024-10-17 15:01:37 +0900

    myCosmeのUI一旦できた

--- frontend/src/app/Home/page.js
+++ frontend/src/app/Home/page.js
@@ -7,22 +7,45 @@ import {useRouter} from "next/navigation";
 import Header from "@/components/header";
 import Post from "@/components/post";
 import s from "@/app/Home/page.module.css"
+import AddTab from "@/components/addTab";
+import {useState} from "react";
 
-const Home = () => {
+const Home = ({pageType}) => {
     const router = useRouter();
 
-    const homeAdditionalFeatures = (
-        <button onClick={() => alert("Home専用機能")}>Homeボタン</button>
-    );
+    const [showAddTab, setShowAddTab] = useState(false);
+    const handleAddClick = () => {
+        console.log("handleAddClick");
+        setShowAddTab(true); // AddTabを表示
+    };
+
+    const handleCloseAddTab = () => {
+        setShowAddTab(false); // AddTabを非表示
+    };
+    const addTab = pageType === 'myCosmetics' ? s.addTabMC : s.addTabHome;
 
     return(
         <>
             <MainLayout>
                 <div className={s.allContainer}>
                     <Header firstTabText="Now" secondTabText="Following" thirdTabText="tab3" firstTabContent={<Post/>}
-                            additionalFeatures={homeAdditionalFeatures} pageType="home"/>
+                            pageType="home"/>
+                    <button className={`${s.addButton} ${addTab}`} onClick={handleAddClick}>+</button>
+                    {/*<button type="button" className={s.addButton} onClick={handleAddClick}>+</button>*/}
                     <PostButton/>
                 </div>
+
+
+                {/* タブ追加ボタン押したとき */}
+                {showAddTab && (
+                    <div className={s.addTabOverlay}>
+                        <div className={s.addTabContent}>
+                            <AddTab pageType={pageType} /> {/* pageTypeを渡す */}
+                            <button onClick={handleCloseAddTab} className={s.buttonCancel}>Cancel</button>
+                        </div>
+                    </div>
+                )}
+
             </MainLayout>
         </>
     )
--- frontend/src/app/MyCosmetics/page.js
+++ frontend/src/app/MyCosmetics/page.js
@@ -3,24 +3,38 @@
 import { useState } from "react";
 import MainLayout from "@/components/MainLayout";
 import s from "./page.module.css";
-import overlay from "@/styles/header.module.css"
+import home from "@/app/Home/page.module.css"
 import Header from "@/components/header";
 import MyCosmeticItems from "@/components/myCosmeticItems";
 
-const MyCosmetics = () => {
+const MyCosmetics = ({ pageType }) => {
+    // state
+    const [showAddTab, setShowAddTab] = useState(false);
     const [isAdding, setIsAdding] = useState(false); // 状態管理用
 
+
+    //Tab追加
+    const handleAddClick = () => {
+        setShowAddTab(true); // AddTabを表示
+    };
+    const handleCloseAddTab = () => {
+        setShowAddTab(false); // AddTabを非表示
+    };
+
+
+    //新規コスメADD
     const handleAddButtonClick = () => {
         setIsAdding(!isAdding); // クリックで状態を切り替え
     };
-
     const handleCancelAdd = () => {
         setIsAdding(false); // AddTabを非表示
     };
-
     const handleAddNewCosmeClick = () => {
         alert("新規コスメ登録のAddボタンをクリックしました")
     }
+
+    // const addTab = pageType === 'myCosmetics' ? home.addTabMC : home.addTabHome;
+
 
     return (
         <>
@@ -29,35 +43,58 @@ const MyCosmetics = () => {
                     {/* header */}
                     <div className={s.headerContainer}>
                         <p className={s.headerText}>My Cosmetics</p>
-                        <Header firstTabText={"カラコン"} secondTabText={"コスメ"} thirdTabText={"♥"} pageType="myCosmetics" />
+                        <Header firstTabText={"カラコン"} secondTabText={"コスメ"} thirdTabText={"♥"}
+                                pageType="myCosmetics"/>
+                        <button className={`${home.addButton}`} style={{ top: '63px' }} onClick={() => handleAddClick()}>+</button>
                     </div>
 
                     <div className={s.searchAndAddContainer}>
-                        {/* search */}
+                    {/* search */}
                         <div className={s.searchContainer}>
-                            <img alt="虫眼鏡アイコン" src="虫眼鏡アイコン.png" className={s.searchImg} />
-                            <input type="search" className={s.searchBox} placeholder="search..." />
+                            <img alt="虫眼鏡アイコン" src="/虫眼鏡アイコン.png" className={s.searchImg}/>
+                            <input type="search" className={s.searchBox} placeholder="search..."/>
                             <button type="button" className={s.searchButton}>Search</button>
                         </div>
                         <button type="button" className={s.addButton} onClick={handleAddButtonClick}>Add</button>
                     </div>
 
                     <div className={s.itemsContainer}>
-                        <MyCosmeticItems />
+                        <MyCosmeticItems/>
                         <MyCosmeticItems />
                          <MyCosmeticItems />
-                         {/*<MyCosmeticItems />*/}
+                         <MyCosmeticItems />
                     </div>
 
+
+
+                    {/* タブ追加ボタン押したとき */}
+                    {showAddTab && (
+                        <div className={home.addTabOverlay}>
+                            <div className={s.addTabContent}>
+                                <div className={s.addTabContainer}>
+                                    <h2 className={s.addTabTitle}>新しいタブを追加する</h2>
+                                    <div className={s.addTabInputContainer}>
+                                        <label className={s.inputTabLabel}>タブのタイトル：<input type="text" className={s.inputTabBox} placeholder="タブのタイトル"/></label>
+                                    </div>
+                                    <div className={s.tabButtonContainer}>
+                                        <button type="button" onClick={handleCloseAddTab}
+                                                className={s.addTabCancel}>キャンセル
+                                        </button>
+                                        <button type="button" className={s.addTabAdd}>追加</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
 
                     {/* Add用のレイアウト */}
                     {isAdding && (
                         <>
-                            <div className={overlay.addTabOverlay}>
+                            <div className={home.addTabOverlay}>
                                 <div className={s.addTabContent}>
                                     <h2 className={s.newCosmeticTitle}>新しいコスメを追加</h2>
                                     <form>
-                                        <div className={s.inputContainer}>
+                                    <div className={s.inputContainer}>
                                             <input type="date" className={s.inputBox} placeholder="開封日" />
                                             <input type="text" className={s.inputBox} placeholder="ブランド" />
                                             <input type="text" className={s.inputBox} placeholder="商品名" />
