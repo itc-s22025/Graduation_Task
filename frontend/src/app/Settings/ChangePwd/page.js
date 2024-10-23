545ddcf8f27abe651dd9fd916afdd8078ead296f
s22006 <s22006@std.it-college.ac.jp>
2024-10-23 17:39:41 +0900

    Settingsを少しいじっった。またSignUpの登録情報とProfileのデータを連携しました

--- frontend/src/app/Settings/ChangePwd/page.js
+++ frontend/src/app/Settings/ChangePwd/page.js
@@ -6,6 +6,7 @@ import AccountHeader from "@/components/AccountHeader";
 import s from "./pwd.module.css";
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 
 const Password = () => {
     const [currentPassword, setCurrentPassword] = useState('');
@@ -13,6 +14,11 @@ const Password = () => {
     const [confirmPassword, setConfirmPassword] = useState('');
     const auth = getAuth();
     const user = auth.currentUser;
+
+    const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
 
     const handleSaveButton = async (e) => {
         e.preventDefault();
@@ -47,6 +53,9 @@ const Password = () => {
             alert("再認証に失敗しました。パスワードが正しいか確認してください。");
         }
     };
+    const toggleConfirmPassword = (setter) => {
+        setter(prevState => !prevState);
+    };
 
     return (
         <>
@@ -57,31 +66,58 @@ const Password = () => {
                         <h2>パスワードを変更してください</h2>
                         <div className={s.box}>
                             <input
-                                type="password"
+                                type={showCurrentPassword ? "text" : "password"}
                                 value={currentPassword}
                                 onChange={(e) => setCurrentPassword(e.target.value)}
                                 placeholder="現在のパスワード"
                                 className={s.text}
+                            />
+                            <Image
+                                src='/eye.png'
+                                alt="i"
+                                width={10}
+                                height={10}
+                                className={s.img}
+                                onClick={() => toggleConfirmPassword(setShowCurrentPassword)}
+                                style={{ cursor: "pointer" }}
                             />
                         </div>
                         <Link href="/Settings/ChangePwd/ForgotPwd" className={s.color}>パスワードをお忘れですか？</Link>
                         <p className={s.border}>-------------------------------------------------------------------------------------------</p>
                         <div className={s.box}>
                             <input
-                                type="password"
+                                type={showCurrentPassword ? "text" : "password"}
                                 value={newPassword}
                                 onChange={(e) => setNewPassword(e.target.value)}
                                 placeholder="新しいパスワード"
                                 className={s.text}
                             />
+                            <Image
+                                src='/eye.png'
+                                alt="i"
+                                width={10}
+                                height={10}
+                                className={s.img}
+                                onClick={() => toggleConfirmPassword(setShowCurrentPassword)}
+                                style={{ cursor: "pointer" }}
+                            />
                         </div>
                         <div className={s.box}>
                             <input
-                                type="password"
+                                type={showCurrentPassword ? "text" : "password"}
                                 value={confirmPassword}
                                 onChange={(e) => setConfirmPassword(e.target.value)}
                                 placeholder="もう一度新しいパスワードを入力"
                                 className={s.text}
+                            />
+                            <Image
+                                src='/eye.png'
+                                alt="i"
+                                width={10}
+                                height={10}
+                                className={s.img}
+                                onClick={() => toggleConfirmPassword(setShowConfirmPassword)}
+                                style={{ cursor: "pointer" }}
                             />
                         </div>
                         <button onClick={handleSaveButton} className={s.save} type="button" id="saveButton">Save</button>
