4e6a16d0bee93c7732fcd6b512e6d86ba538a466
s22025 <s22025@std.it-college.ac.jp>
2024-10-30 16:14:33 +0900

    なんかSignUpの機能が色々メインから消えてたので無理やりプッシュ

--- frontend/src/app/SignUp/detail.js
+++ frontend/src/app/SignUp/detail.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import s from './detail.module.css';
+
 import { useRouter } from "next/navigation";
 //firebase
 import { auth, db } from '@/firebase';
