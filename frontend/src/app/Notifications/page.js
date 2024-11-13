ee73758f5a0067317226d0d90630fb7caf68285e
itc-s22016 <s22016@std.it-college.ac.jp>
2024-11-13 12:02:39 +0900

    通知一部完成

--- frontend/src/app/Notifications/page.js
+++ frontend/src/app/Notifications/page.js
@@ -1,10 +1,11 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 import { db } from "@/firebase";
 import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import MainLayout from "@/components/MainLayout";
 import s from "./page.module.css";
+import post from "@/components/post";
 
 const Notifications = () => {
     const [notifications, setNotifications] = useState([]);
@@ -27,12 +28,17 @@ const Notifications = () => {
     return (
         <div>
             <MainLayout>
-            <h2 className={s.title}>通知</h2>
-            {notifications.map(notification => (
-                <div className={s.naiyou} key={notification.id}>
-                    <p>{notification.message}</p>
-                </div>
-            ))}
+                <h1 className={s.title}>通知</h1>
+                {notifications.map(notifications => (
+                    <div className={s.box} key={notifications.id}>
+                        <div className={s.naiyou}>
+                            <img src="/iine.png" className={s.iine}/>
+                            <p>{notifications.message}</p>
+                            <p className={s.tweet}>{notifications.tweet}</p> {/* 投稿内容を表示 */}
+
+                        </div>
+                    </div>
+                ))}
             </MainLayout>
         </div>
     );
