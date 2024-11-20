2582098c43d39b635026c2a60a8cb3e54824d74a
s22025 <s22025@std.it-college.ac.jp>
2024-11-20 14:07:05 +0900

    修正分むりやりpush

--- frontend/src/app/Profile/page.js
+++ frontend/src/app/Profile/page.js
@@ -1,8 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import { getFirestore, doc, getDoc, updateDoc} from "firebase/firestore";
-import { useRouter } from 'next/navigation';
 import s from './profile.module.css';
 import MainLayout from "@/components/MainLayout";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
@@ -11,9 +9,7 @@ import Edit from '@/app/Profile/edit'
 import Link from "next/link";
 import Post from "@/components/post";
 import ReviewPosts from "@/components/ReviewPost";
-import {doc, getDoc, getDocs, updateDoc, where, query, collection} from "firebase/firestore";
-import {db, auth} from "@/firebase"
-import { onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, getDoc, getDocs, updateDoc, where, query, collection} from "firebase/firestore";
 import {useRouter} from "next/navigation";
 
 
