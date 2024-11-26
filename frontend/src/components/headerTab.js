d9a6db066ea66cd72177486d7946759800b766ba
s22025 <s22025@std.it-college.ac.jp>
2024-11-26 16:50:18 +0900

    tabの色

--- frontend/src/components/headerTab.js
+++ frontend/src/components/headerTab.js
@@ -1,11 +1,15 @@
 import s from "@/styles/headerTab.module.css";
+import home from '@/app/Home/page.module.css'
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { useEffect, useState } from "react";
 import Post from "@/components/post";
 import { db } from "@/firebase";
-import { collection, query, where, getDocs, doc, getDoc } from "firebase/firestore";
+import {collection, query, where, getDocs, doc, getDoc, orderBy, addDoc} from "firebase/firestore";
+import AddTab from "@/components/addTab";
 
 const HeaderTab = ({ user }) => {
+    //state
+    const [showAddTab, setShowAddTab] = useState(false);
     const [tabs, setTabs] = useState([
         { id: "now", title: "Now", content: <div><Post /></div> },
         { id: "following", title: "Following", content: <div>Loading...</div> },
@@ -40,7 +44,8 @@ const HeaderTab = ({ user }) => {
                 // フォロー中のユーザーの投稿を取得
                 const postsQuery = query(
                     collection(db, "posts"),
-                    where("uid", "in", following)
+                    where("uid", "in", following),
+                    orderBy("timestamp", "desc")
                 );
                 const postsSnap = await getDocs(postsQuery);
                 console.log("following::", following);
@@ -86,15 +91,29 @@ const HeaderTab = ({ user }) => {
         setFocusedTab(tabId);
     };
 
-    const handleAddTab = () => {
-        const newTabId = `tab${tabs.length + 1}`;
-        const newTab = {
-            id: newTabId,
-            title: `New Tab ${tabs.length + 1}`,
-            content: <div>New Content for Tab {tabs.length + 1}</div>,
-        };
+    const handleAddTab = async () => {
+        setShowAddTab(true); // AddTabを表示
+        // const newTabId = `tab${tabs.length + 1}`;
+        // const newTab = {
+        //     id: newTabId,
+        //     title: `New Tab ${tabs.length + 1}`,
+        //     content: <div>New Content for Tab {tabs.length + 1}</div>,
+        // };
 
-        setTabs([...tabs, newTab]);
+        // Firestoreに新しいタブを追加
+        // try {
+        //     const docRef = await addDoc(collection(db, "homesTab"), {
+        //         id: newTabId,
+        //         title: newTab.title,
+        //         content: `New Content for Tab ${tabs.length + 1}`,
+        //     });
+        //
+        //     // Firestoreに保存したタブを状態に追加
+        //     setTabs([...tabs, newTab]);
+        //     console.log("新しいタブがFirestoreに追加されました:", docRef.id);
+        // } catch (error) {
+        //     console.error("タブの追加に失敗しました:", error);
+        // }
     };
 
     return (
@@ -107,7 +126,17 @@ const HeaderTab = ({ user }) => {
                                 key={tab.id}
                                 className={`${s.tabs} ${focusedTab === tab.id ? s.zIndex4 : ''}`}
                                 tabIndex={0}
+                                // tabIndex={focusedTab === 'all' ? 0 : index + 1}  // "All" タブを一番最初に表示
                                 onFocus={() => handleFocus(tab.id)}
+                                style={{
+                                    zIndex: focusedTab === tab.id
+                                        ? 4  // フォーカスしているタブが最前面
+                                        : (tab.id === 'following' ? 2 : 1),  // favタブは2、他は1
+                                    backgroundColor: tab.id === "now" ? "#fff"
+                                        : tab.id === "following" ? "#FFDCDD" : "#FFBFC0", //背景色
+                                    color: tab.id === "now" ? "#FF989A" : tab.id === "following" ? "#FF989A" : "#fff",  //文字色
+                                    borderBottom: tab.id === "following" ? "none" : tab.id === "tab3" ? "none" : ""
+                                }}
                             >
                                 {tab.title}
                             </Tab>
@@ -124,6 +153,17 @@ const HeaderTab = ({ user }) => {
                     </TabPanel>
                 ))}
             </Tabs>
+
+
+            {/* タブ追加ボタン押したとき */}
+                {showAddTab && (
+                    <div className={home.addTabOverlay}>
+                        <div className={home.addTabContent}>
+                            <AddTab />
+                            {/*<button onClick={handleCloseAddTab} className={s.buttonCancel}>Cancel</button>*/}
+                        </div>
+                    </div>
+                )}
         </>
     );
 };
