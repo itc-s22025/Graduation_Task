27a5dd33f29fce53cc0a485eff2616e8ae3939a8
s22025 <s22025@std.it-college.ac.jp>
2024-12-05 12:10:33 +0900

    一旦コミット

--- frontend/src/app/s22025/page.js
+++ frontend/src/app/s22025/page.js
@@ -3,15 +3,18 @@
 import s from './page.module.css'
 import MyCosmeticsHeaderTab from "@/components/myCosmeticsHeaderTab";
 import MainLayout from "@/components/MainLayout";
+import FirstLayout from "@/components/FirstLayout"
+import {Spring} from "@/components/pcResult";
 
 
 const Test = () => {
     return(
         <>
-            <MainLayout>
-                <MyCosmeticsHeaderTab/>
-                <h1 className={s.hello}>hello test</h1>
-            </MainLayout>
+            <FirstLayout>
+                {/*<MyCosmeticsHeaderTab/>*/}
+                {/*<h1 className={s.hello}>hello test</h1>*/}
+                <Spring/>
+            </FirstLayout>
         </>
     )
 }
